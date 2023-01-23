21cbf0bab73684a3acf407cfc8d735b31d180ae7
Victor <victor.santos.cd@gmail.com>
2023-01-23 09:00:39 -0300

    feat(dags.processing): Creates the dag processing

--- dags/processing.py
+++ dags/processing.py
@@ -3,19 +3,19 @@ from airflow.providers.postgres.operators.postgres import PostgresOperator
 
 from datetime import datetime
 
-with DAG('processing', start_date=datetime(2023, 1, 22), 
-    schedule_interval='@daily', catchup=False) as dag:
+with DAG(dag_id='processing', start_date=datetime(2022, 1, 1), 
+        schedule_interval='@daily', catchup=False) as dag:
 
     create_table = PostgresOperator(
-        task_id="create_table",
-        postgres_conn_id="postgres",
-        sql=""""
-        CREATE TABLE IF NOT EXISTS users (
-            first_name TEXT NOT NULL,
-            last_name TEXT NOT NULL,
-            username TEXT NOT NULL,
-            password TEXT NOT NULL,
-            email TEXT NOT NULL,
-        )
+        task_id='create_table',
+        postgres_conn_id='postgres',
+        sql="""
+            CREATE TABLE IF NOT EXISTS users (
+                firstname TEXT NOT NULL,
+                lastname TEXT NOT NULL,
+                username TEXT NOT NULL,
+                password TEXT NOT NULL,
+                email TEXT NOT NULL
+            );
         """
     )
