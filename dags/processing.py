e8386f8c62228b8eff5469bb98ee7120b1a1dd9f
Victor <victor.santos.cd@gmail.com>
2023-02-11 12:57:43 -0300

    feat(dags.processing): Create the task extract_user_info

--- dags/processing.py
+++ dags/processing.py
@@ -1,8 +1,10 @@
 from airflow import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.http.operators.http import SimpleHttpOperator
 
 from datetime import datetime
+import json
 
 with DAG(dag_id='processing', start_date=datetime(2022, 1, 1), 
         schedule_interval='@daily', catchup=False) as dag:
@@ -25,4 +27,14 @@ with DAG(dag_id='processing', start_date=datetime(2022, 1, 1),
         task_id="is_api_available",
         http_conn_id="user_api",
         endpoint="api/"
+    )
+
+    extract_user_info = SimpleHttpOperator(
+        task_id="extract_user_info",
+        http_conn_id="user_api",
+        endpoint="api/",
+        method="GET",
+        response_filter=lambda response: json.loads(response.text),
+        log_response=True
+
     )
