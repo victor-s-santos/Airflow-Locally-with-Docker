af66046d0ee3c6daf47b01d62a829f278f53fe74
Victor <victor.santos.cd@gmail.com>
2023-02-11 12:37:42 -0300

    feat(dags.processing): Add the is_api_avaliable task

--- dags/processing.py
+++ dags/processing.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.http.sensors.http import HttpSensor
 
 from datetime import datetime
 
@@ -18,4 +19,10 @@ with DAG(dag_id='processing', start_date=datetime(2022, 1, 1),
                 email TEXT NOT NULL
             );
         """
+    )
+
+    is_api_avaliable = HttpSensor(
+        task_id="is_api_available",
+        http_conn_id="user_api",
+        endpoint="api/"
     )
